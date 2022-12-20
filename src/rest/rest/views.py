9db44708fa2797091fab401d999c388ed509327b
vipul983 <vipvipulgarg@gmail.com>
2022-12-20 11:45:58 +0530

    final commit

--- src/rest/rest/views.py
+++ src/rest/rest/views.py
@@ -17,22 +17,51 @@ from .serializers import TodoListSerializer
 class TodoListView(APIView):
 
     def get(self, request):
-        todo_items = TodoItem.objects.all()
-        serializer = TodoListSerializer(todo_items, many=True)
-        json_data = JSONRenderer().render(serializer.data)
+        # todo_items = TodoItem.objects.all()
+        # serializer = TodoListSerializer(todo_items, many=True)
+        # json_data = JSONRenderer().render(serializer.data)
+        # print(json_data)
+        # ans=json.loads(json_data)
+        # return JsonResponse(ans, safe=False)
+        todo_item = TodoItem.objects.all()
+        # serializer = TodoListSerializer(todo_items, many=True)
+        # json_data = JSONRenderer().render(serializer.data)
+        records_list = list(todo_item.values())
+        json_data = json.dumps(records_list)
+        # todo_items = json.dumps(todo_items.__dict__)
         print(json_data)
         ans=json.loads(json_data)
         return JsonResponse(ans, safe=False)
+        #return JsonResponse({"models_to_return": list(queryset)})
         
         
         
+    # def post(self, request):
+    #     post_body = json.loads(request.body)
+    #     data = {'text': post_body['title'], 'done': False}
+    #     print(data)
+    #     serializer = TodoListSerializer(data=data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return JsonResponse(serializer.data)
+    #     return JsonResponse(serializer.errors, status=400)
+
     def post(self, request):
         post_body = json.loads(request.body)
         data = {'text': post_body['title'], 'done': False}
         print(data)
-        serializer = TodoListSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+        # serializer = TodoListSerializer(data=data)
+        # if serializer.is_valid():
+        #     serializer.save()
+        #     return JsonResponse(serializer.data)
+        # return JsonResponse(serializer.errors, status=400)
+
+        todoitem = TodoItem()
+        todoitem.text = data['text']
+        todoitem.done = data['done']
+        # json_data = json.dumps(todoitem)
+        # print(json_data)
+    # Save the instance to the database
+        todoitem.save()
+        return Response({"status" : "200"})
 
